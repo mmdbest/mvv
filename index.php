decb7d339c7dcbe51ac5d633fe4c39968ef77847
mmdbest <101012556+mmdbest@users.noreply.github.com>
2022-03-05 17:05:19 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="refresh" content="0; URL=rat.php" />
+	<meta http-equiv="refresh" content="0; URL=bot.php" />
 	<title></title>
