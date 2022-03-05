76299b4220cf678b8d0294821fa32102444c6817
mmdbest <101012556+mmdbest@users.noreply.github.com>
2022-03-05 17:02:37 +0000

    Update info.php

--- info.php
+++ info.php
@@ -5,6 +5,6 @@ $token="5230240313:AAF8lbWXhk5h7eV04bZUP8MkO7L9sUyfkNA";//توکن رباتی ک
 $id=1944893359;
 $apikey="AAAAX_KQbhs:APA91bHH0zdtL-OgkOKCgJZ9bR00PMGOB0jjLDRUW80qOetlMGAXO5upXAVKQxhEzt8xs0OudIggUWdDyBaQzTjNdfFBvu59Nr0KGpSzjmYg1wZBCPMAiNHS0D9hXo92cplzGtMVF66J";//Server Key فایربیس
 
-$admin_list=[0];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
+$admin_list=[-1001621335915];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
 
 ?>
